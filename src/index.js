d1cfb79ab6477602881b2ca856e731b182e4b61b
DarshanaCV <133231903+DarshanaCV@users.noreply.github.com>
2024-05-04 22:03:26 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import reducer from './components/reducers'; 
+import localStorageMiddleware from './components/localStorageMiddleware';
+import './index.css';
+import App from './App';
+
+const initialState={
+  tasks: JSON.parse(localStorage.getItem('tasks')) || []
+};
+
+const setupMiddleware=()=>{
+  return [localStorageMiddleware];
+};
+
+const store=configureStore({
+  reducer: reducer,
+  middleware: (getDefaultMiddleware) => setupMiddleware(),
+  preloadedState: initialState
+});
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
